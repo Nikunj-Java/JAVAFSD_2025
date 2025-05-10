23a83bef0542b2d4b21162320a4efb59841e7b94
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-05-10 19:34:34 +0530

    Angular

---
+++ Phase-1/Session-17/first.js
@@ -0,0 +1,11 @@
+var a;
+a = 36;
+console.log("a is: ", a);
+var b = 12;
+console.log("b is: ", b);
+var fname = "Nikunj Soni";
+console.log("name: ", fname);
+function test(num1, num2) {
+    return num1 + num2;
+}
+console.log("Addition of Two Numbers: ", test(5, 3));
---
+++ Phase-1/Session-17/first.ts
@@ -0,0 +1,20 @@
+let a:number;
+a=36;
+
+console.log("a is: ",a);
+
+let b:number=12;
+
+console.log("b is: ",b);
+
+let fname:string="Nikunj Soni";
+
+console.log("name: ",fname);
+
+function test(num1:number,num2:number){
+    return num1+num2;
+}
+
+console.log("Addition of Two Numbers: ",test(5,3));
+
+
